b0051d27348b7b3a3b627b9aecd6fd6331319a80
ashiba713 <ashibaalben7744@gmail.com>
2025-12-22 02:28:57 +0530

    Update career_data.py

---
+++ career_data.py
@@ -0,0 +1,26 @@
+CAREERS = {
+    "Software Engineer": {
+        "effort": 7,
+        "risk": 5,
+        "stability": 7,
+        "burnout": 6
+    },
+    "Government Job": {
+        "effort": 8,
+        "risk": 2,
+        "stability": 9,
+        "burnout": 4
+    },
+    "Higher Studies": {
+        "effort": 8,
+        "risk": 6,
+        "stability": 6,
+        "burnout": 5
+    },
+    "Startup": {
+        "effort": 9,
+        "risk": 9,
+        "stability": 3,
+        "burnout": 9
+    }
+}
